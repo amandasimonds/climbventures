abf56b004db52773cedb2672e9047e54322e4b82
Amanda Simonds <amandasimonds9@gmail.com>
2023-07-17 15:24:21 -0400

    added main pages, and rock climbing video because i decided to change the theme of this website

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+	return (
+		<section className="home">
+			<div className="overlay"></div>
+			<video src=""></video>
+		</section>
+	);
+};
+
+export default Home;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import { MdOutlineModeOfTravel } from 'react-icons/md';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { PiDotsNineBold } from 'react-icons/pi';
+
+export const Navbar = () => {
+	const [active, setActive] = useState('navbar');
+	const showNav = () => {
+		setActive('navbar active-navbar');
+	};
+
+	const hideNav = () => {
+		setActive('navbar');
+	};
+	return (
+		<section className="navbar-section">
+			<header className="header flex">
+				<div className="logo-div">
+					<a href="#" className="logo flex">
+						<h1>
+							<MdOutlineModeOfTravel className="icon" /> Travel.
+						</h1>
+					</a>
+				</div>
+
+				<div className={active}>
+					<ul className="navlists flex">
+						<li className="nav-item">
+							<a href="#" className="nav-link">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#" className="nav-link">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#" className="nav-link">
+								Shop
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#" className="nav-link">
+								News
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="#" className="nav-link">
+								Contact
+							</a>
+						</li>
+						<button className="btn">
+							<a href="#">Book now</a>
+						</button>
+					</ul>
+					<div onClick={hideNav} className="close-navbar">
+						<AiFillCloseCircle className="icon" />
+					</div>
+				</div>
+
+				<div onClick={showNav} className="toggle-navbar">
+					<PiDotsNineBold className="icon" />
+				</div>
+			</header>
+		</section>
+	);
+};
